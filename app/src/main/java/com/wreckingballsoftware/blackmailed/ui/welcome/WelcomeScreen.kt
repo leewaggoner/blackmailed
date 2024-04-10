dd258410e18daa0fb4b5a0abaf48d3efbba76f0b
Lee Waggoner <lwaggoner@roadrunner.com>
2024-04-10 11:22:23 -0700

    First pass at welcome screen. Added dimensions and custom typeography

--- app/src/main/java/com/wreckingballsoftware/blackmailed/ui/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/wreckingballsoftware/blackmailed/ui/welcome/WelcomeScreen.kt
@@ -2,15 +2,22 @@ package com.wreckingballsoftware.blackmailed.ui.welcome
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.wreckingballsoftware.blackmailed.R
 import com.wreckingballsoftware.blackmailed.ui.navigation.NavGraph
+import com.wreckingballsoftware.blackmailed.ui.theme.blackmailedTypography
+import com.wreckingballsoftware.blackmailed.ui.theme.dimensions
 
 @Composable
 fun WelcomeScreen(
@@ -31,9 +38,21 @@ fun WelcomeScreenContent(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Text("Welcome to Blackmailed")
+        Text(
+            text = stringResource(id = R.string.welcome),
+            style = MaterialTheme.blackmailedTypography.headline,
+        )
+        Text(
+            text = stringResource(id = R.string.app_name) + "!",
+            style = MaterialTheme.blackmailedTypography.headlineLarge)
+        Spacer(modifier = Modifier.height(MaterialTheme.dimensions.spaceLarge))
+        Text(
+            text = stringResource(id = R.string.press_play),
+            style = MaterialTheme.blackmailedTypography.title,
+        )
+        Spacer(modifier = Modifier.height(MaterialTheme.dimensions.spaceMedium))
         Button(onClick = { navGraph.navigateToGamePlayScreen() }) {
-            Text("Start")
+            Text(text = stringResource(id = R.string.play))
         }
     }
 }
